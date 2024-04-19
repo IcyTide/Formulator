0f5c288d05d600eb7c5926bf638003c7c46add50
ango <wangbingchaofun@gmail.com>
2024-04-19 18:03:40 +0800

    04.19 commit

--- schools/bei_ao_jue/gains.py
+++ schools/bei_ao_jue/gains.py
@@ -1,5 +1,5 @@
-from base.recipe import damage_addition_recipe
-from general.gains.equipment import EQUIPMENT_GAINS, EquipmentGain, CriticalSet
+from base.recipe import damage_addition_recipe, critical_strike_recipe
+from general.gains.equipment import EQUIPMENT_GAINS, CriticalSet
 from base.gain import Gain
 
 
@@ -10,9 +10,12 @@ GAINS = {
     4291: damage_addition_recipe([16803, 16802, 16801, 16800, 17043, 19423, 19424, 32859], 51),
     4294: damage_addition_recipe([16610], 51),
     4295: damage_addition_recipe([16760, 16382], 51),
+    4296: critical_strike_recipe([16760, 16382], 500),
+    4297: Gain(),
     2430: Gain(),
     1942: Gain(),
     17374: Gain(),
+    17375: Gain(),
     17239: Gain(),
     **EQUIPMENT_GAINS,
 }
