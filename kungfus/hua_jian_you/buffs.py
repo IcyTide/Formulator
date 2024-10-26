900570e7ac9c27fb44879ca2ece92603347cd6d9
IcyGrain <wangbingchaofun@163.com>
2024-10-26 14:23:10 +0800

    10.26 commit

--- kungfus/hua_jian_you/buffs.py
+++ kungfus/hua_jian_you/buffs.py
@@ -5,7 +5,8 @@ from base.buff import Buff
 BUFFS: Dict[int, Dict[type, Dict[int, dict]]] = {
     0: {
         Buff: {
-            1487: {}, 11809: {}, 28116: {},
+            11809: {}, 28116: {},
+            1487: dict(max_stack=1),
             -9722: dict(interval=320),
             24599: dict(begin_frame_shift=-2),
             12588: dict(buff_name="清流", continuous=True),
