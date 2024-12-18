6d3aa80377e75cdb580928870a63a17899a70a86
IcyGrain <wangbingchaofun@163.com>
2024-12-18 18:21:23 +0800

    12.18 commit

--- kungfus/zhou_tian_gong/gains.py
+++ kungfus/zhou_tian_gong/gains.py
@@ -3,7 +3,7 @@ from general.buffs import GENERAL_BUFFS
 from general.gains.equipment import EQUIPMENT_GAINS, CriticalSet
 
 GAINS = {
-    (2726,): CriticalSet(GENERAL_BUFFS[1438]),
+    (2726,): CriticalSet(GENERAL_BUFFS[29597]),
     (2724,): Gain(),
     (2725,): Gain(),
     (2723,): Gain(),
